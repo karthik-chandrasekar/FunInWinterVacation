5bcbf532df3f1d28fe526793ddec406e9182e438
Karthik Chandrasekar <karthidec1@gmail.com>
2014-01-14 02:07:47 -0700

    Final accepted code for zig zag traversal

--- ZigZagTraversal.java
+++ ZigZagTraversal.java
@@ -1,9 +1,22 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+
 public class Solution {
       public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
     Stack<TreeNode> stack1 = new Stack<TreeNode>();
     Stack<TreeNode> stack2 = new Stack<TreeNode>();
     ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
     ArrayList<Integer> innerOutput = new ArrayList<Integer>();
+    TreeNode top;
+    int flag = 0;
 
     if (root == null)
     {
@@ -14,30 +27,65 @@ public class Solution {
     innerOutput.add(root.val);
     output.add(innerOutput);
     
-    while(false == stack1.isEmpty() && false == stack2.isEmpty())
+    while(!stack1.isEmpty() || !stack2.isEmpty())
     {
-        
-        while (false == stack1.isEmpty())
+        innerOutput = new ArrayList<Integer>();
+        flag = 0;
+        while (!stack1.isEmpty())
         {
-            innerOutput.clear();
-            stack2.push(stack1.peek().left);
-            innerOutput.add(stack1.peek().left.val);
-            innerOutput.add(stack1.peek().right.val);
-            output.add(innerOutput);
-            stack2.push(stack1.peek().right);
+            
+            
+            top = stack1.peek();
+            if (top.right != null)
+            {
+                flag = 1;
+                stack2.push(top.right);
+                innerOutput.add(top.right.val);
+            }
+            
+            if (top.left != null)
+            {
+                flag = 1;
+                stack2.push(top.left);
+                innerOutput.add(top.left.val);
+            }
+            
+            
             stack1.pop();
             
         }
-        while (false == stack2.isEmpty())
+        if (flag == 1)
+            {
+                output.add(innerOutput);
+            }
+        
+        innerOutput = new ArrayList<Integer>();
+        flag = 0;
+        while (!stack2.isEmpty())
         {
-                innerOutput.clear();
-                stack1.push(stack2.peek().right);
-                stack1.push(stack2.peek().left);
-                innerOutput.add(stack1.peek().right.val);
-                innerOutput.add(stack1.peek().left.val);
-                output.add(innerOutput);    
+                
+                
+
+                top = stack2.peek();
+                if (top.left != null)
+                {
+                    flag = 1;
+                    stack1.push(top.left);
+                    innerOutput.add(top.left.val);
+                }
+                if (top.right!=null)
+                {
+                    flag = 1;
+                    stack1.push(top.right);
+                    innerOutput.add(top.right.val);
+                }
+               
                 stack2.pop();
         }
+         if (flag == 1)
+                {
+                    output.add(innerOutput);    
+                }
         
     }
     return output;

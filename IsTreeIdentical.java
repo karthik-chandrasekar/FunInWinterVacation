33e5fce1a657901d0e32b68e0b0c5398dd9d8383
Karthik Chandrasekar <karthidec1@gmail.com>
2014-01-14 20:45:15 -0700

    IsTreeIdentical.java

---
+++ IsTreeIdentical.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if( p == null && q == null)
+        {
+            return true;
+        }
+        
+        if ((p == null && q != null) || (p != null && q == null))
+        {
+            return false;
+        }
+        if (p.val != q.val)
+        {
+            return false;
+        }
+        
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right) ;
+    }
+}
